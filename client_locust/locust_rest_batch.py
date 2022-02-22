c0ab0e8c1784fcff4179819cef3c1cfac6e81e08
dmuiruri <denonjugush@gmail.com>
2022-02-22 22:35:09 +0000

    fixed the double inferences previously created during data preparation

--- client_locust/locust_rest_batch.py
+++ client_locust/locust_rest_batch.py
@@ -14,20 +14,24 @@ import json
 import sys
 import subprocess
 import os
+import tensorflow as tf
+
 
 from locust import HttpUser, FastHttpUser, task, tag, between, stats, run_single_user
 import mnist_input_data
 
 stats.CSV_STATS_INTERVAL_SEC = 1 # default is 1 second
 stats.CSV_STATS_FLUSH_INTERVAL_SEC = 10 # Determines how often the data is flushed to disk, default is 10 seconds
-
+stats.PERCENTILES_TO_REPORT = [25, 50, 75, 95]
 work_dir = './tmp'
 
 test_data_set = mnist_input_data.read_data_sets(work_dir).test
 batch_size = int(os.environ['BATCHSIZE'])
-img_l, label_l = test_data_set.next_batch(batch_size)
-batch = np.repeat(img_l, batch_size, axis=0).tolist()
-json_data_l = {
+img, labels = test_data_set.next_batch(batch_size)
+batch = np.repeat(img, 1, axis=0).tolist()
+# print(f'labels: {labels}, image size: {img.shape}, image type: {type(img)}, batch size: {len(batch)}')
+
+json_data = {
     "signature_name": 'predict_images',
     "instances": batch
 }
@@ -45,7 +49,9 @@ class restClientBatch(HttpUser):
         """
         Get prediction in a batch
         """
-        response_prediction = self.client.post(':8501/v1/models/mnist:predict', json=json_data_l)
+        response_prediction = self.client.post(':8501/v1/models/mnist:predict', json=json_data)
+        # inference = response_prediction.json()['predictions']
+        # print(f"batchsize: {len(inference)}, predictions: {[np.argmax(inf) for inf in inference]}")
         return
 
 if __name__ == "__main__":
